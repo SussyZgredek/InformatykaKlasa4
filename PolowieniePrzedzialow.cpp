2fcf5db87400c0b4304e5cc077a1ea225bb41acb
SussyZgredek <148342724+SussyZgredek@users.noreply.github.com>
2026-03-27 15:42:39 +0100

    Update PolowieniePrzedzialow.cpp

--- PolowieniePrzedzialow.cpp
+++ PolowieniePrzedzialow.cpp
@@ -1,54 +1,30 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    float a = -10.0f;
-    float b = 10.0f;
-    float epsilon = 0.00001f;
+    float a = -10;
+    float b = 10;
+    float epsilon = 0.00001;
     float srodek;
-    float fa, fb, fs;
 
-    fa = a * (a * (a - 3) + 2) - 6;
-    fb = b * (b * (b - 3) + 2) - 6;
-
-    if (fa == 0)
+    while (b - a > epsilon)
     {
-        cout << "Znalezione miejsce zerowe wynosi: " << a << endl;
-    }
-    else if (fb == 0)
-    {
-        cout << "Znalezione miejsce zerowe wynosi: " << b << endl;
-    }
-    else
-    {
-        while (b - a > epsilon)
-        {
-            srodek = (a + b) / 2;
-            fs = srodek * (srodek * (srodek - 3) + 2) - 6;
-            fa = a * (a * (a - 3) + 2) - 6;
-
-            if (fs == 0)
-            {
-                break;
-            }
-
-            if (fa * fs < 0)
-            {
-                b = srodek;
-            }
-            else
-            {
-                a = srodek;
-            }
-        }
-
         srodek = (a + b) / 2;
 
-        cout << fixed << setprecision(5);
-        cout << "Znalezione miejsce zerowe wynosi: " << srodek << endl;
+        if ((a * (a * (a - 3) + 2) - 6) * (srodek * (srodek * (srodek - 3) + 2) - 6) < 0)
+        {
+            b = srodek;
+        }
+        else
+        {
+            a = srodek;
+        }
     }
+
+    srodek = (a + b) / 2;
+
+    cout << "Znalezione miejsce zerowe wynosi: " << srodek << endl;
 
     return 0;
 }
